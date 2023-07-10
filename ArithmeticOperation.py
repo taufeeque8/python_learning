d27ef33cfa27fba1e00ddf7ffde8b1961b6462d2
taufeeque8 <taufeeque8@gmail.com>
2023-07-10 22:42:45 +0530

    first commit

---
+++ ArithmeticOperation.py
@@ -0,0 +1,24 @@
+#Addition
+a,b = 2,3
+sum = a+b
+
+print(sum)
+
+## Substraction ##
+subtract = a - b
+print(subtract)
+
+## Multiplication
+product = a*b
+print(product)
+
+# division (/,//,%)
+# /  -> always return float
+# //  --> discard the fractional part
+# % --> returns the remainder
+
+print(a/b)
+print(a // b)
+print(a %b )
+
+# floats have full support,
